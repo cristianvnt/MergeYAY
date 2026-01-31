9d604d8ba155caac12a84b6cfd26f64f9c4fe94a
Mematoru <mematoru@gmail.com>
2026-01-31 11:46:46 +0200

    simple rectangles in grid

--- src/Common.h
+++ src/Common.h
@@ -7,6 +7,7 @@ constexpr int SCREEN_WIDTH = 600;
 constexpr int SCREEN_HEIGHT = 900;
 
 constexpr int GRID_CELL_SIZE = 64;
+constexpr int GRID_CELL_BORDER = 3;
 constexpr int MAX_GRID_CELLS_X = 5;
 constexpr int MAX_GRID_CELLS_Y = 10;
 
--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,6 @@
 #include "Common.h"
 
 #include <string>
-#include <iostream>
 
 int main()
 {
@@ -13,20 +12,34 @@ int main()
 
 	Vector2 gridPosition = { GetScreenWidth() / 2.f - GRID_CELL_SIZE * MAX_GRID_CELLS_X / 2.f, GetScreenHeight() / 2.f - GRID_CELL_SIZE * MAX_GRID_CELLS_Y / 2.f };
 
+
 	while (!WindowShouldClose())
 	{
+		// UPDATE
+		// ---------------------------
+		
+		// ---------------------------
+
+		// DRAW
+		// ---------------------------
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
 
 		if (IsWindowResized())
 			gridPosition = { GetScreenWidth() / 2.f - GRID_CELL_SIZE * MAX_GRID_CELLS_X / 2.f, GetScreenHeight() / 2.f - GRID_CELL_SIZE * MAX_GRID_CELLS_Y / 2.f };
 
+		// grid
 		for (int i = 0; i <= MAX_GRID_CELLS_X; ++i)
 			DrawLine(gridPosition.x + i * GRID_CELL_SIZE, gridPosition.y, gridPosition.x + i * GRID_CELL_SIZE, gridPosition.y + MAX_GRID_CELLS_Y * GRID_CELL_SIZE, BLUE);
 		for (int i = 0; i <= MAX_GRID_CELLS_Y; ++i)
 			DrawLine(gridPosition.x, gridPosition.y + i * GRID_CELL_SIZE, gridPosition.x + MAX_GRID_CELLS_X * GRID_CELL_SIZE, gridPosition.y + i * GRID_CELL_SIZE, RED);
 
+		// rectangles
+		for (int i = 0; i < MAX_GRID_CELLS_X; ++i)
+			DrawRectangle(gridPosition.x + i * GRID_CELL_SIZE + GRID_CELL_BORDER, gridPosition.y + GRID_CELL_BORDER, GRID_CELL_SIZE - 2 * GRID_CELL_BORDER, GRID_CELL_SIZE - 2 * GRID_CELL_BORDER, PINK);
+
 		EndDrawing();
+		// --------------------------
 	}
 
 	CloseWindow();
